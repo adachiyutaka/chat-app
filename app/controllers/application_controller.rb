53ce526a48b10b1efe6cb4a7a601734587b54f7e
adachiyutaka <adachi.yutaka@gmail.com>
2020-07-14 16:58:22 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  #before_action :move_to_index
+  before_action :configure_permitted_parameters, if: :devise_controller?
+	
+	protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  
+  private
+  # def move_to_index
+  #   redirect_to new_user_registration_path unless user_signed_in?
+  # end
 end
