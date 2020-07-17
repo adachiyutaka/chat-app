8f8f63afb00072f34369622cfe283169ca85d404
adachiyutaka <adachi.yutaka@gmail.com>
2020-07-17 15:43:54 +0900

    モデルテストの修正

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -1,20 +1,5 @@
-require 'rails_helper'
-
-RSpec.describe Room, type: :model do
-  describe '#create' do
-    before do
-      @room = FactoryBot.build(:room)
-    end
-
-    it "nameの値が存在すれば登録できること" do
-      expect(@room).to be_valid
-    end
-
-    it "nameが空では登録できないこと" do
-      @room.name = nil
-      @room.valid?
-      expect(@room.errors.full_messages).to include("Name can't be blank")
-    end
+FactoryBot.define do
+  factory :room do
+    name {Faker::Team.name}
   end
-
 end
